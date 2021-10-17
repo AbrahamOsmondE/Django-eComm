aaa6c9ef58d381d048f0a9ce6ed677d8116ab056
Abraham Osmond Edmundo <abraham.osmond@gmail.com>
2021-10-17 17:58:20 +0800

    django auth

--- magnacarta/settings.py
+++ magnacarta/settings.py
@@ -131,3 +131,15 @@ STATICFILES_DIRS = ['magnacarta/static',]
 #media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+#SMTP CONFIGURATION
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'magnacarta192@gmail.com'
+EMAIL_HOST_PASSWORD = 'KonJancokKonJancok12345678!@#$%^&*'
+EMAIL_USE_TLS = True
