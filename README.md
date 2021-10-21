5cb30f8af55dc331e5528b22b1592dd569664e71
Abraham Osmond Edmundo <abraham.osmond@gmail.com>
2021-10-21 23:17:56 +0800

    allowed host

---
+++ README.md
@@ -0,0 +1,16 @@
+DJANGO E-COMMERCE PROJECT
+
+Majority of functionality only applied to ATX Jeans product
+
+Implemented a fully functional ecommerce website,  shopping cart functionality, product gallery, rating/review system, receipt functionality
+
+Implemented a login system that requires a verification link to be clicked for the account to be active, as well as a change password functionality
+Implemented paypal payment gateway, and a secure way to pay using credit/debit card.
+
+Utilized python decouple to store website’s sensitive information
+
+Implemented secure django admin portal using honeypot
+
+Logs out inactive user using django session timeout, which logs out after 1 hour of inactivity
+
+Deployed the website on AWS Elastic Beanstalk, and implemented AWS RDS Postgres as database.
--- accounts/admin.py
+++ accounts/admin.py
@@ -1,16 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Account
+from django.utils.html import format_html
+from .models import Account, UserProfile
 
 class AccountAdmin(UserAdmin):
-    list_display = ('email','first_name','last_name','username','last_login','date_join','is_active')
-    list_display_links = ('email','first_name','last_name')
-    readonly_fields = ('last_login','date_join')
+    list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_join', 'is_active')
+    list_display_links = ('email', 'first_name', 'last_name')
+    readonly_fields = ('last_login', 'date_join')
     ordering = ('-date_join',)
 
     filter_horizontal = ()
     list_filter = ()
     fieldsets = ()
 
+class UserProfileAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="30" style="border-radius:50%;">'.format(object.profile_picture.url))
+    thumbnail.short_description = 'Profile Picture'
+    list_display = ('thumbnail', 'user', 'city', 'state', 'country')
+
 admin.site.register(Account,AccountAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
 # Register your models here.
